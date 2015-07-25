6ffa5fb8ede4714272d47a35eb3d905f9b019e4c
Andrii Pidhirnyi <andrii_pidhirnyi@epam.com>
2015-07-25 10:45:18 +0300

    Create project structure

---
+++ assets/js/all.js
@@ -0,0 +1,13 @@
+/**
+ * Summarizes two floats.
+ *
+ * @param {number} val1   The first component
+ * @param {number} val2   The second component
+ * @return {number}
+ */
+function summator(val1, val2) {
+  var first = parseFloat(val1, 10);
+  var second = parseFloat(val2, 10);
+
+  return first + second;
+}
